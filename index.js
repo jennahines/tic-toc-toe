3d820b23ecc2aa8135943be094a974e4de984ee9
Jenna Hines <jenna.hines@webdevstudios.com>
2022-07-11 11:37:07 -0400

    refactor

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-
 const player = (name, token) => {
 
-	const getToken = () => {
-		return token;
-	}
 	const getName = () => {
 		return name;
 	}
 
-	return { name, token };
+	const getToken = () => {
+		return token;
+	}
+	
+	return { getName, getToken };
 };
-
 
 const gameBoard = (() => {
 	const board = ['', '', '', '', '', '', '', '', ''];
 
 	const setField = (index, token) => {
-		console.log(`set ${index} to ${token}`);
 		board[index] = token;
 	}
 
@@ -38,6 +36,10 @@ const gameBoard = (() => {
 const displayController = (() => {
 	const $spaces = document.querySelectorAll('.game-board-space');
 	const $resetButton = document.getElementById('reset-button');
+
+	const $player1 = document.querySelector('[data-player-id="1"]');
+	const $player2 = document.querySelector('[data-player-id="2"]');
+	const $message = document.querySelector('[data-message]');
 
 	for (const space of $spaces) {
 		space.addEventListener('click', (e) => {
@@ -66,7 +68,24 @@ const displayController = (() => {
 		}
 	};
 
-	
+	const setMessage = (message) => {
+		if ( message ) {
+			$message.innerText = message
+		} else {
+			$message.innerText = '';
+		}
+	}
+
+	const setPlayerInfo = (p1, p2) => {
+		$player1.querySelector('[data-name]').innerText = p1.getName();
+		$player1.querySelector('[data-token]').innerText = p1.getToken();
+
+		$player2.querySelector('[data-name]').innerText = p2.getName();
+		$player2.querySelector('[data-token]').innerText = p2.getToken();
+
+	}
+
+	return { setPlayerInfo, setMessage };
 })();
 
 
@@ -74,57 +93,38 @@ const gameController = (() => {
 	const p1 = player('Player 1', 'x');
 	const p2 = player('Player 2', 'o');
 	let round = 1;
-	let currentP = p1;
-	
-	const $player1 = document.querySelector('[data-player-id="1"]');
-	const $player2 = document.querySelector('[data-player-id="2"]');
-	const $currentPlayer = document.querySelector('[data-player-name]');
-	
-	const updateCurrentPlayer = () => {
-		if ( 0 === round % 2 ) {
-			currentP = p2;
-		} else {
-			currentP = p1;
-		}
-		$currentPlayer.innerText = getCurrentPlayerName();
-		console.log(currentP);
-		console.log(getCurrentPlayerToken());
-
-	}
-
-	const getCurrentPlayer = () => {
-		console.log(currentP);
-		return currentP;
-	}
 
 	const getCurrentPlayerToken = () => {
-		return currentP.token;
+		return round % 2 === 1 ? p1.getToken() : p2.getToken();
 	}
 	
 	const getCurrentPlayerName = () => {
-		return currentP.name;
+		return round % 2 === 1 ? p1.getName() : p2.getName();
 	}
 
 	const playGame = (index) => {
+		checkWinner(index);
 		gameBoard.setField(index, getCurrentPlayerToken());
 		round++;
-		updateCurrentPlayer();
+		displayController.setMessage(`${getCurrentPlayerName()}'s Turn`);
+	}
+
+	const checkWinner = (index) => {
+		// TODO: determine winner
+		console.log('checking winner');
 	}
 
 	const resetGame = () => {
 		round = 1;
-		updateCurrentPlayer();
+		displayController.setMessage(`${getCurrentPlayerName()}'s Turn`);
 	}
 
-	updateCurrentPlayer();
+	displayController.setPlayerInfo(p1, p2);
+	displayController.setMessage(`${getCurrentPlayerName()}'s Turn`);
 
-	$player1.querySelector('[data-name]').innerText = p1.name;
-	$player1.querySelector('[data-token]').innerText = p1.token;
-	
-	$player2.querySelector('[data-name]').innerText = p2.name;
-	$player2.querySelector('[data-token]').innerText = p2.token;
-
-	return { playGame, resetGame };
+	return { playGame, resetGame, getCurrentPlayerName };
 
 })();
+
+
 
